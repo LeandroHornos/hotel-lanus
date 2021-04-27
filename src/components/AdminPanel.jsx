be0faa0b3d2cb4f5a85b11c4f00dd3509a690559
Lean <leandrohornos@gmail.com>
2021-04-27 11:18:37 -0300

    asigno precios a camas y las numero

--- src/components/AdminPanel.jsx
+++ src/components/AdminPanel.jsx
@@ -12,6 +12,7 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Alert from "react-bootstrap/Alert";
 import Spinner from "react-bootstrap/Spinner";
+import InputGroup from "react-bootstrap/InputGroup";
 
 const AdminPanel = () => {
   return (
@@ -38,9 +39,19 @@ const RoomEditor = () => {
   // State
   const [name, setName] = useState("");
   const [shortDescription, setShortDescription] = useState("");
+  const [longDescription, setLongDescription] = useState("");
   const [roomType, setRoomType] = useState("shared");
   const [singleBeds, setSingleBeds] = useState(0);
   const [doubleBeds, setDoubleBeds] = useState(0);
+  const [singleBedPrice, setSingleBedPrice] = useState({
+    price: 0,
+    currency: "ar$",
+  });
+  const [doubleBedPrice, setDoubleBedPrice] = useState({
+    price: 0,
+    currency: "ar$",
+  });
+  const [roomPrice, setRoomPrice] = useState({ price: 0, currency: "ar$" });
   const [gender, setGender] = useState("mixed");
   const [imageAsFile, setImageAsFile] = useState("");
   const [errorMsgs, setErrorMsgs] = useState([]);
@@ -70,23 +81,23 @@ const RoomEditor = () => {
       });
       dataIsValid = false;
     }
-    // if (mainImgUrl === "") {
-    //   errors.push({
-    //     field: "mainImgUrl",
-    //     error: "La habitación debe contar con una foto",
-    //   });
-    //   dataIsValid = false;
-    // }
+    if (imageAsFile === "") {
+      errors.push({
+        field: "imageAsFile",
+        error: "La habitación debe contar con una foto",
+      });
+      dataIsValid = false;
+    }
     return { dataIsValid, errors };
   };
 
   const createBedDocs = (singleBeds, doubleBeds, roomId) => {
     let bedDocs = [];
     for (let i = 0; i < singleBeds; i++) {
-      bedDocs.push({ roomId, type: "single", subType: "", customId: "" });
+      bedDocs.push({ roomId, type: "single", subType: "", customId: `S-${i}` });
     }
     for (let i = 0; i < doubleBeds; i++) {
-      bedDocs.push({ roomId, type: "double", subType: "", customId: "" });
+      bedDocs.push({ roomId, type: "double", subType: "", customId: `D-${i}` });
     }
     return bedDocs;
   };
@@ -99,10 +110,13 @@ const RoomEditor = () => {
       let roomData = {
         name,
         shortDescription,
-        longDescription: "",
+        longDescription,
         roomType,
         singleBeds,
         doubleBeds,
+        singleBedPrice,
+        doubleBedPrice,
+        roomPrice,
         gender,
         mainImgUrl: "",
         imgUrls: [],
@@ -125,10 +139,12 @@ const RoomEditor = () => {
           .ref("images")
           .child(imageAsFile.name)
           .getDownloadURL();
+
         // Guardo la habitación
         roomData = { ...roomData, mainImgUrl };
         const room = await db.collection("rooms").add(roomData);
         console.log("se ha creado una habitacion", room.id);
+
         // Guardo las camas
         const beds = createBedDocs(singleBeds, doubleBeds, room.id);
         beds.forEach((bed) => {
@@ -211,16 +227,54 @@ const RoomEditor = () => {
           }}
         >
           <option value="mixed">Mixta</option>
-          <option value="female">Dormi Femenino</option>
-          <option value="male">Dormi Masculino</option>
+          <option value="female">Femenina</option>
+          <option value="male">Masculina</option>
         </Form.Control>
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Label>Descripción corta</Form.Label>
+        <Form.Text>
+          Escribe una descripción corta de la habitación que de al usuario una
+          idea de las características más importantes de la habitación. Esta
+          información se presenta en la vista previa de las habitaciones
+        </Form.Text>
+        <Form.Control
+          type="text"
+          as="textarea"
+          value={shortDescription}
+          rows={2}
+          placeholder="Describe resumidamente la habitación"
+          onChange={(e) => {
+            setShortDescription(e.target.value);
+          }}
+        />
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Label>Descripción larga</Form.Label>
+        <Form.Text>
+          Detalla aquí la descripción completa de la habitación. Esta
+          información se muestra cuando el usuario selecciona una habitación en
+          particular para conocer todos sus detalles
+        </Form.Text>
+        <Form.Control
+          type="text"
+          as="textarea"
+          value={longDescription}
+          rows={6}
+          placeholder="Describe detalladamente la habitación"
+          onChange={(e) => {
+            setLongDescription(e.target.value);
+          }}
+        />
       </Form.Group>
 
       <Form.Group>
         <Form.Label>Camas</Form.Label>
         <div className="row">
           <div className="col-6">
-            <Form.Text>Individuales</Form.Text>
+            <Form.Text>Simples</Form.Text>
             <Form.Control
               type="number"
               value={singleBeds}
@@ -232,7 +286,7 @@ const RoomEditor = () => {
             />
           </div>
           <div className="col-6">
-            <Form.Text>Matrimoniales</Form.Text>
+            <Form.Text>Dobles</Form.Text>
             <Form.Control
               type="number"
               value={doubleBeds}
@@ -247,18 +301,127 @@ const RoomEditor = () => {
       </Form.Group>
 
       <Form.Group>
-        <Form.Label>Descripción</Form.Label>
-        <Form.Control
-          type="text"
-          as="textarea"
-          value={shortDescription}
-          rows={4}
-          placeholder="Describe la habitación"
-          onChange={(e) => {
-            setShortDescription(e.target.value);
-          }}
-        />
+        <Form.Label>Precio de las camas</Form.Label>
+        <Form.Text>
+          Si la habitacipón es compartida, se asignan los precios según el tipo
+          de cama
+        </Form.Text>
+        <InputGroup className="mb-3">
+          <InputGroup.Prepend>
+            <InputGroup.Text
+              id="basic-addon3"
+              className={roomType === "private" ? "text-muted" : ""}
+            >
+              Cama simple por día
+            </InputGroup.Text>
+          </InputGroup.Prepend>
+          <Form.Control
+            type="number"
+            value={singleBedPrice.price}
+            aria-describedby="Single bed price"
+            disabled={roomType === "private"}
+            onChange={(e) => {
+              setSingleBedPrice({ ...singleBedPrice, price: parseFloat(e.target.value) });
+            }}
+          />
+          <InputGroup.Append>
+            <Form.Control
+              as="select"
+              value={singleBedPrice.currency}
+              disabled={roomType === "private"}
+              className={roomType === "private" ? "text-muted" : ""}
+              onChange={(e) => {
+                setSingleBedPrice({
+                  ...singleBedPrice,
+                  currency: e.target.value,
+                });
+              }}
+            >
+              <option value="ar$">AR$</option>
+              <option value="usd">USD</option>
+            </Form.Control>
+          </InputGroup.Append>
+        </InputGroup>
+        <InputGroup className="mb-3">
+          <InputGroup.Prepend>
+            <InputGroup.Text
+              id="basic-addon3"
+              className={roomType === "private" ? "text-muted" : ""}
+            >
+              Cama doble por día{"  "}
+            </InputGroup.Text>
+          </InputGroup.Prepend>
+          <Form.Control
+            type="number"
+            value={doubleBedPrice.price}
+            aria-describedby="Double bed price"
+            disabled={roomType === "private"}
+            onChange={(e) => {
+              setDoubleBedPrice({ ...doubleBedPrice, price: parseFloat(e.target.value) });
+            }}
+          />
+          <InputGroup.Append>
+            <Form.Control
+              as="select"
+              value={doubleBedPrice.currency}
+              disabled={roomType === "private"}
+              className={roomType === "private" ? "text-muted" : ""}
+              onChange={(e) => {
+                setDoubleBedPrice({
+                  ...doubleBedPrice,
+                  currency: e.target.value,
+                });
+              }}
+            >
+              <option value="ar$">AR$</option>
+              <option value="usd">USD</option>
+            </Form.Control>
+          </InputGroup.Append>
+        </InputGroup>
       </Form.Group>
+
+      <Form.Group>
+        <Form.Label>Precio de la habitación</Form.Label>
+        <Form.Text>
+          Si la habitacipón es privada, se le asigna un precio a la habitación
+          completa
+        </Form.Text>
+        <InputGroup className="mb-3">
+          <InputGroup.Prepend>
+            <InputGroup.Text
+              id="basic-addon3"
+              className={roomType === "shared" ? "text-muted" : ""}
+            >
+              Habitación por día
+            </InputGroup.Text>
+          </InputGroup.Prepend>
+          <Form.Control
+            type="number"
+            value={roomPrice.price}
+            id="basic-url"
+            aria-describedby="basic-addon3"
+            disabled={roomType === "shared"}
+            onChange={(e) => {
+              setRoomPrice({ ...roomPrice, price: parseFloat(e.target.value) });
+            }}
+          />
+          <InputGroup.Append>
+            <Form.Control
+              as="select"
+              value={roomPrice.currency}
+              disabled={roomType === "shared"}
+              className={roomType === "shared" ? "text-muted" : ""}
+              onChange={(e) => {
+                setRoomPrice({ ...roomPrice, currency: e.target.value });
+              }}
+            >
+              <option value="ar$">AR$</option>
+              <option value="usd">USD</option>
+            </Form.Control>
+          </InputGroup.Append>
+        </InputGroup>
+      </Form.Group>
+
       <Form.Group>
         <Form.Label>Foto de portada</Form.Label>
         <Form.File
